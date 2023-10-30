246cf1f826aa0b5d235b8e1ee4c3c3a82bb5621a
Valcheez <97644134+Valcheez@users.noreply.github.com>
2023-10-30 16:35:25 -0400

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,3 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-#Use this from the python shell to initialize the db
-# from app import app, db
-# with app.app_context():
-#     db.create_all()
